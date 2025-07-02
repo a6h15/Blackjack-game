9545b9a16be99dcdd2783a85b4ff68b8a63c8c28
Abhishek Singh <abhi.r1509.git@gmail.com>
2025-07-02 13:21:58 +0530

    final commit

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,5 @@
 import art,random
-print(art.logo)
+
 
 def deal_card():
     """Returns a random card from the deck"""
@@ -18,32 +18,62 @@ def calculate_score(cards):
 
     return sum(cards)
 
-player_cards = []
-computer_cards = []
-comp_score = -1 #for the while loop that plays as the computer.
-player_score = -1
-game_over = False
-
-for _ in range(2): # '_' cuz we don't need the variable, just need to run this loop twice.
-    player_cards.append(deal_card())
-    computer_cards.append(deal_card())
-
-while not game_over:
-    player_score = calculate_score(player_cards)
-    comp_score = calculate_score(computer_cards)
-
-    print(f"Your cards : {player_cards} ,current score : {player_score}")
-    print(f"Computer first card : {computer_cards[0]}")
-
-    if player_score == 0 or comp_score == 0 or player_score > 21: #0 as when the sum is 21 we return 0 , thats the condition
-            game_over = True
+def compare(p_score, c_score):
+    """Compares the user score player_score against the computer score comp_score."""
+    if p_score == c_score:
+        return "Draw 🙃"
+    elif c_score == 0:
+        return "Lose, opponent has Blackjack 😱"
+    elif p_score == 0:
+        return "Win with a Blackjack 😎"
+    elif p_score > 21:
+        return "You went over. You lose 😭"
+    elif c_score > 21:
+        return "Opponent went over. You win 😁"
+    elif p_score > c_score:
+        return "You win 😃"
     else:
-        player_deals = input(f"Type 'y' to take another card or type 'n' to pass")
-        if player_deals == "y":
-            player_cards.append(deal_card())
-        else:
-            game_over = True
+        return "You lose 😤"
 
-while comp_score != 0 and comp_score < 17:
-    computer_cards.append(deal_card())
-    comp_score = calculate_score(computer_cards)
+
+def game_loop():
+    print(art.logo)
+    player_cards = []
+    computer_cards = []
+    comp_score = -1 #for the while loop that plays as the computer.
+    player_score = -1
+    game_over = False
+
+    for _ in range(2): # '_' cuz we don't need the variable, just need to run this loop twice.
+        player_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    while not game_over:
+        player_score = calculate_score(player_cards)
+        comp_score = calculate_score(computer_cards)
+
+        print(f"Your cards : {player_cards} ,current score : {player_score}")
+        print(f"Computer first card : {computer_cards[0]}")
+
+        if player_score == 0 or comp_score == 0 or player_score > 21: #0 as when the sum is 21 we return 0 , that's the condition
+                game_over = True
+        else:
+            player_deals = input(f"Type 'y' to take another card or type 'n' to pass")
+            if player_deals == "y":
+                player_cards.append(deal_card())
+            else:
+                game_over = True
+
+    while comp_score != 0 and comp_score < 17:
+        computer_cards.append(deal_card())
+        comp_score = calculate_score(computer_cards)
+
+
+    print(f"Your final hand: {player_cards}, final score: {player_score}")
+    print(f"Computer's final hand: {computer_cards}, final score: {comp_score}")
+    print(compare(player_score, comp_score))
+
+while input("Do you want to play another game of BlackJack : 'y' or 'n'") == 'y':
+    print("\n"*20)
+    game_loop()
+
